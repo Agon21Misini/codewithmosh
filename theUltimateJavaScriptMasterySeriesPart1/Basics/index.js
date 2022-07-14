9941a69692fcd40657c4141928cebfcb9de452c7
Agon21Misini <misiniagon80@gmail.com>
2022-07-14 16:53:21 +0200

    Constants

--- theUltimateJavaScriptMasterySeriesPart1/Basics/index.js
+++ theUltimateJavaScriptMasterySeriesPart1/Basics/index.js
@@ -1,10 +1,3 @@
-let name = "Agon";
-console.log(name);
-
-// Cannot be a reserved keyword
-// Should be meaningful
-// cannot start with a number (1name)
-// cannot contain a space or hyphen (-)
-// Are case-sensitive
-let firstName = "Agon";
-let lastName = "Misini";
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
