6e5cae66b7affc9f3ff363555eac21bba309ba94
Agon21Misini <misiniagon80@gmail.com>
2022-07-14 16:50:55 +0200

    Variables

---
+++ theUltimateJavaScriptMasterySeriesPart1/Basics/index.js
@@ -0,0 +1,10 @@
+let name = "Agon";
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// cannot start with a number (1name)
+// cannot contain a space or hyphen (-)
+// Are case-sensitive
+let firstName = "Agon";
+let lastName = "Misini";
