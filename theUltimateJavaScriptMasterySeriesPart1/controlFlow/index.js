921ab462729535e46270e2e1384813836ffd2fb8
Agon21Misini <misiniagon80@gmail.com>
2022-07-15 16:01:13 +0200

    Exercise 5- Even and Odd Numbers

--- theUltimateJavaScriptMasterySeriesPart1/controlFlow/index.js
+++ theUltimateJavaScriptMasterySeriesPart1/controlFlow/index.js
@@ -1,13 +1,10 @@
-checkSpeed(130);
-
-function checkSpeed(speed) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-  if (speed <= speedLimit + kmPerPoint) {
-    console.log("OK");
-    return;
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? "EVEN" : "ODD";
+    console.log(i, message);
+    // if (i % 2 === 0) console.log(i, "EVEN");
+    // else console.log(i, "ODD");
   }
-  const points = Math.floor((speed - speedLimit) / kmPerPoint);
-  if (points >= 12) console.log("License suspended");
-  else console.log("Points", points);
 }
+
+showNumbers(20);
