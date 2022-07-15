d683ca69360c57ef9afc4f0ecefc2092e444fa2f
Agon21Misini <misiniagon80@gmail.com>
2022-07-15 14:08:24 +0200

    Bitwise Operators

--- theUltimateJavaScriptMasterySeriesPart1/Operators/index.js
+++ theUltimateJavaScriptMasterySeriesPart1/Operators/index.js
@@ -1,16 +1,19 @@
-// Falsy (false)
-// undefined
-// null
-// 0
-// false
-// ''
-// NaN
+// 1 = 00000001
+// 2 = 00000010
+// R = 00000011 = 3
+// r = 00000000 = 0
 
-// Anything that is not Falsy -> Truthy
+// Read, Write, Execute
+// 00000100, 00000010, 00000001
+// console.log(1 | 2); // Bitwise OR
+// console.log(1 & 2); // Bitwise AND
 
-// Short-Circuiting
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
 
-let userColor = undefined;
-let defaultColor = "blue";
-let currentColor = userColor || defaultColor;
-console.log(currentColor);
+let myPermission = 0;
+myPermission = myPermission | writePermission;
+
+let message = myPermission & readPermission ? "yes" : "no";
+console.log(message);
