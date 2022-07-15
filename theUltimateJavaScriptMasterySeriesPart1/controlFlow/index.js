371e87c4d7b79ad645bc8d588f4d7140ec40515f
Agon21Misini <misiniagon80@gmail.com>
2022-07-15 16:25:42 +0200

    Exercise 9- Grade

--- theUltimateJavaScriptMasterySeriesPart1/controlFlow/index.js
+++ theUltimateJavaScriptMasterySeriesPart1/controlFlow/index.js
@@ -1,11 +1,19 @@
-console.log(sum(20));
+const marks = [80, 80, 50];
+console.log(calculateGrade(marks));
 
-function sum(limit) {
-  let count = 0;
+function calculateGrade(marks) {
+  const average = calculateAverage(marks);
+  if (average < 60) return "F";
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return "A";
+}
 
-  for (let i = 0; i <= limit; i++) {
-    if (i % 3 === 0 || i % 5 === 0) count += i;
+function calculateAverage(array) {
+  let sum = 0;
+  for (let value of marks) {
+    sum += value;
   }
-
-  return count;
+  return sum / marks.length;
 }
