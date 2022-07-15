9a8515c260e49d77533fca11696d52967b2c8283
Agon21Misini <misiniagon80@gmail.com>
2022-07-15 14:09:54 +0200

    Operators Precedence

--- theUltimateJavaScriptMasterySeriesPart1/Operators/index.js
+++ theUltimateJavaScriptMasterySeriesPart1/Operators/index.js
@@ -1,19 +1,3 @@
-// 1 = 00000001
-// 2 = 00000010
-// R = 00000011 = 3
-// r = 00000000 = 0
-
-// Read, Write, Execute
-// 00000100, 00000010, 00000001
-// console.log(1 | 2); // Bitwise OR
-// console.log(1 & 2); // Bitwise AND
-
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
-
-let myPermission = 0;
-myPermission = myPermission | writePermission;
-
-let message = myPermission & readPermission ? "yes" : "no";
-console.log(message);
+// let x = 2 + 3 * 4;
+let x = (2 + 3) * 4;
+console.log(x);
