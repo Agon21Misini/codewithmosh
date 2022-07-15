8854325b30705b1cd16efdec7c10e272df92faee
Agon21Misini <misiniagon80@gmail.com>
2022-07-15 14:27:46 +0200

    Switch...case

--- theUltimateJavaScriptMasterySeriesPart1/controlFlow/index.js
+++ theUltimateJavaScriptMasterySeriesPart1/controlFlow/index.js
@@ -1,21 +1,17 @@
-// Hour
-// if hour is between 6am and 12pm: Good morning!
-// if it is between 12pm and 6pm: Good afternoon!
-// Otherwise: Good evening!
+// let role;
+// let role = "guest";
+let role = "moderator";
+switch (role) {
+  case "guest":
+    console.log("Guest User");
+    break;
+  case "moderator":
+    console.log("Moderator User");
+    break;
+  default:
+    console.log("Unknown User");
+}
 
-// if (condition) {
-//   statement;
-// } else if (anotherCondition) {
-//   statement;
-// } else if (yetAnotherCondition) {
-//   statement;
-// } else {
-//   statement;
-// }
-// let hour = 10;
-// let hour = 15;
-let hour = 23;
-
-if (hour >= 6 && hour < 12) console.log("Good morning!");
-else if (hour >= 12 && hour < 18) console.log("Good afternoon!");
-else console.log("Good evening!");
+if (role === "guest") console.log("Guest User");
+else if (role === "moderator") console.log("Moderator User");
+else console.log("Unknown User");
