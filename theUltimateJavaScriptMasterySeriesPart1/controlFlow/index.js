5f6e10084dcdccd5dfe8450eec53a1a54068574f
Agon21Misini <misiniagon80@gmail.com>
2022-07-15 15:45:57 +0200

    Exercise 3- FizzBuzz

--- theUltimateJavaScriptMasterySeriesPart1/controlFlow/index.js
+++ theUltimateJavaScriptMasterySeriesPart1/controlFlow/index.js
@@ -1,5 +1,21 @@
-function isLandscape(width, height) {
-  return width > height;
-}
+const output = fizzBuzz(11);
+console.log(output);
+function fizzBuzz(input) {
+  let dividedBy3 = input % 3 === 0;
+  let dividedBy5 = input % 5 === 0;
+  let dividedByBoth = dividedBy3 && dividedBy5;
+  if (typeof input !== "number") {
+    return "Not a number";
+  }
+  if (dividedByBoth) {
+    return "FizzBuzz";
+  }
+  if (dividedBy3) {
+    return "Fizz";
+  }
+  if (dividedBy5) {
+    return "Buzz";
+  }
 
-console.log(isLandscape(300, 600));
+  return input;
+}
